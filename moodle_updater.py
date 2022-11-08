5fc8d755169041f10db12ef2d957dd63632a8c32
d4 <d4@unpx.net>
2022-11-08 23:11:13 +0100

    Remove global variables and create a main

--- moodle_updater.py
+++ moodle_updater.py
@@ -39,45 +39,16 @@ def die(*a):
     print(*a, file=sys.stderr)
     exit(1)
 
-# load config, halt otherwise
-config_file = None
-parser = argparse.ArgumentParser(prog = 'moodle_updater',
-                                 description = 'I watch moodle and report on telegram')
-parser.add_argument('-c', dest='config_file', help="")
-parsed = parser.parse_args()
-if parsed.config_file == None:
-    parser.print_help()
-    exit(1)
-with open(parsed.config_file, mode="rb") as fp:
-    config = tomli.load(fp)
-course_id = config['course_id']
-guest_pass = config['guest_pass']
-root_url = config['root_url']
-telegram_token = config['telegram_token']
-telegram_channel = config['telegram_channel']
-save_file = config['save_file']
-if course_id == None:
-    die("Required course_id config variable")
-elif guest_pass == None:
-    die("Required guest_pass config variable")
-elif root_url == None:
-    die("Required root_url config variable")
-elif telegram_token == None:
-    die("Required telegram_token config variable")
-elif save_file == None:
-    die("Required save_file config variable")
-
-# begin of funny functions
-
-def fetch_new_list():
+## begin of funny functions
+def fetch_new_list(root_url, course_id, guest_pass):
     s = requests.Session()
     r = s.get(root_url + '/course/view.php?id=' + course_id)
     parsed_html = BeautifulSoup(r.text, 'html.parser')
     form = parsed_html.body.find('form', attrs={'class': 'mform', 'method': 'post'})
     url = form['action']
     payload = {}
-    for i in form.find_all('input'):
-        payload[i['name']] = i['value']
+    for input_tag in form.find_all('input'):
+        payload[input_tag['name']] = input_tag['value']
     payload['guestpassword'] = guest_pass
     # performs login and it will be redirected to the course we want to monitor
     r = s.post(url, data=payload)
@@ -90,20 +61,23 @@ def fetch_new_list():
             activities.append(list(instanceTag.stripped_strings)[0])
     return activities
 
-def load_list():
-    if os.path.exists(save_file) and os.path.isfile(save_file):
-        with open(save_file, "r") as f:
+def load_list(filename):
+    try:
+        with open(filename, "r") as f:
             lines = f.read().splitlines()
             return lines
-    else:
+    except Exception as e:
+        print('Was not able to read ' + filename ', will try to create one.')
         return []
 
-def save_list(l):
-    with open(save_file, "w") as f:
-        f.write('\n'.join(l))
+def save_list(filename, new_list):
+    try:
+        with open(filename, "w") as f:
+            f.write('\n'.join(new_list))
+    except Exception:
+        die('Was not able to write ' + filename + ' file.')
 
-# Begin of diff stuff
-
+## Begin of diff stuff
 # Adapted from https://rosettacode.org/wiki/Longest_common_subsequence#Dynamic_Programming_7
 def lcs(a, b):
     lengths = [[0] * (len(b)+1) for _ in range(len(a)+1)]
@@ -149,26 +123,59 @@ def diff(a, b):
     return d
 
 # Pretty print for screen
-def pretty_print_diff(d):
-    return '\n'.join([('+' if l[0] else '-') + ' ' + l[1] for l in d])
+def pretty_print_diff(deltas):
+    return '\n'.join([('+' if line[0] else '-') + ' ' + line[1] for line in deltas])
 
 # Send to telegram channel
-def send_message(text):
+def send_message(telegram_token, telegram_channel, text):
     payload = {'chat_id': telegram_channel, 'text': text}
-    r = requests.post('https://api.telegram.org/bot' + telegram_token + '/sendMessage', data=payload)
+    r = requests.post('https://api.telegram.org/bot' + ctx.telegram_token + '/sendMessage', data=payload)
     # print(r.text)
 
-# Old test for diff algo
-# new = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'i', 'j', 'k', 'r', 'x', 'y', 'z', 'u']
-# old = ['a', 'b', 'c', 'd', 'f', 'g', 'h', 'j', 'q', 'z']
-# print(pretty_print_diff(diff(old, new)))
+# Test for diff algo
+def test_diff():
+    new = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'i', 'j', 'k', 'r', 'x', 'y', 'z', 'u']
+    old = ['z', 'a', 'b', 'c', 'd', 'f', 'g', 'h', 'j', 'q', 'z']
+    print(pretty_print_diff(diff(old, new)))
 
-# Should I wrap this into a main function?
-new = fetch_new_list()
-old = load_list()
-d = diff(old, new)
-if len(d) > 0:
-    # only if we have a difference we will send the message
-    send_message(pretty_print_diff(d))
-    save_list(new)
+if __name__ == "__main__":
+    # load config, halt otherwise
+    config_file = None
+    parser = argparse.ArgumentParser(prog = 'moodle_updater',
+                                    description = 'I watch moodle and report on telegram')
+    parser.add_argument('-c', dest='config_file', help="")
+    parsed = parser.parse_args()
+    if parsed.config_file == None:
+        parser.print_help()
+        exit(1)
+    try:
+        with open(parsed.config_file, mode="rb") as fp:
+            toml_config = tomli.load(fp)
+    except:
+        die('Could not open ' + parsed.config_file)
+    course_id        = toml_config['course_id']
+    guest_pass       = toml_config['guest_pass']
+    root_url         = toml_config['root_url']
+    telegram_token   = toml_config['telegram_token']
+    telegram_channel = toml_config['telegram_channel']
+    save_file        = toml_config['save_file']
+    if course_id == None:
+        die("Required course_id config variable")
+    elif guest_pass == None:
+        die("Required guest_pass config variable")
+    elif root_url == None:
+        die("Required root_url config variable")
+    elif telegram_token == None:
+        die("Required telegram_token config variable")
+    elif save_file == None:
+        die("Required save_file config variable")
+    new = fetch_new_list(root_url, course_id, guest_pass)
+    old = load_list(save_file)
+    d = diff(old, new)
+    if len(d) > 0:
+        # only if we have a difference we will send the message
+        send_message(telegram_token, telegram_channel, pretty_print_diff(d))
+        if os.getenv('DEBUG') != None:
+            print(pretty_print_diff(d))
+        save_list(save_file, new)
 
